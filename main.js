9decef6d8cf5599b948948ed99390bc14858da71
monkeclicker <83822524+monkeclicker@users.noreply.github.com>
2021-07-22 17:23:25 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,436 @@
-function enterGame(){document.getElementById("welcome").style.display="none"}!function(){var e=window.requestAnimationFrame||window.mozRequestAnimationFrame||window.webkitRequestAnimationFrame||window.msRequestAnimationFrame;window.requestAnimationFrame=e}(),document.getElementById("playGame").addEventListener("click",enterGame);var canvas=document.getElementById("canvas"),ctx=canvas.getContext("2d"),width=document.body.clientWidth,height=document.body.clientHeight,player={x:50,y:height-200,width:50,height:50,speed:3,velX:0,velY:0,jumping:!1,grounded:!1,color:"#E6AC27",image:document.getElementById("character")},level=1,keys=[],friction=.8,gravity=.4,boxes=[],boxes2=[],powerup=[],lava=[];function update(){if((keys.ArrowUp||keys.Space||keys.KeyW)&&!player.jumping&&player.grounded&&(player.jumping=!0,player.grounded=!1,player.velY=2.5*-player.speed),(keys.ArrowRight||keys.KeyD)&&player.velX<player.speed&&player.velX++,(keys.ArrowLeft||keys.KeyA)&&player.velX>-player.speed&&player.velX--,player.velX*=friction,player.velY+=gravity,ctx.clearRect(0,0,width,height),player.grounded=!1,ctx.beginPath(),1==level){ctx.font="normal 40px Montserrat",ctx.fillText("Level 1",canvas.width/2-50,canvas.height/2-200);for(var e=0;e<boxes.length;e++){ctx.fillStyle=boxes[e].color,ctx.fillRect(boxes[e].x,boxes[e].y,boxes[e].width,boxes[e].height),"l"===(a=collisionCheck(player,boxes[e]))||"r"===a?(player.velX=0,player.jumping=!1):"b"===a?(player.grounded=!0,player.jumping=!1):"t"===a&&(player.velY*=-1)}for(var t=0;t<lava.length;t++){var a;ctx.fillStyle=lava[t].color,ctx.fillRect(lava[t].x,lava[t].y,lava[t].width,lava[t].height),(a=collisionCheck(player,lava[t]))&&(console.log("touched"),player.x=50,player.y=height-200,alert("You Died!"),document.location.reload())}player.grounded&&(player.velY=0),player.x+=player.velX,player.y+=player.velY,ctx.drawImage(player.image,player.x,player.y,player.width,player.height);for(var r=0;r<powerup.length;r++){ctx.save();var l=powerup[r].x+.5*powerup[r].width,o=powerup[r].y+.5*powerup[r].height;if(ctx.translate(l,o),ctx.rotate(Math.PI/180*45),"tele"===powerup[r].effect&&(ctx.rotate(Math.PI/180*powerup[r].rotate),powerup[r].rotate=Math.PI/180*powerup[r].rotate),ctx.translate(-l,-o),ctx.fillStyle=powerup[r].color,ctx.fillRect(powerup[r].x,powerup[r].y,powerup[r].width,powerup[r].height),ctx.restore(),null!==collisionCheck(player,powerup[r])){if("gravity"===powerup[r].effect)gravity=.4,player.speed=4,player.color="white";else if("shrink"===powerup[r].effect)player.width=10,player.height=10,player.speed=5;else if("tele"===powerup[r].effect)player.x=powerup[r].px,player.y=powerup[r].py;else if("win"===powerup[r].effect){alert("You beat level 1!");player.x=50,player.y=height-200,player.width=50,player.height=50,player.speed=3,player.velX=0,player.velY=0}!0!==powerup[r].stay&&(powerup[r].width=0),powerup[r].height=0}}requestAnimationFrame(update)}}function collisionCheck(e,t){var a=e.x+e.width/2-(t.x+t.width/2),r=e.y+e.height/2-(t.y+t.height/2),l=e.width/2+t.width/2,o=e.height/2+t.height/2,i=null;if(Math.abs(a)<l&&Math.abs(r)<o){var h=l-Math.abs(a),p=o-Math.abs(r);h>=p?r>0?(i="t",e.y+=p):(i="b",e.y-=p):a>0?(i="l",e.x+=h):(i="r",e.x-=h)}return i}powerup.push({x:490,y:height-195,width:20,height:20,color:"#BF4D28",effect:"shrink"}),powerup.push({x:60,y:415,width:20,height:20,color:"#2A5D77",effect:"win"}),boxes.push({x:0,y:0,width:10,height:height,color:"#a08f73"}),boxes.push({x:0,y:height-50,width:300,height:50,color:"#a08f73"}),boxes.push({x:width-10,y:0,width:50,height:height,color:"#a08f73"}),boxes.push({x:0,y:0,width:width,height:10,color:"#a08f73"}),boxes.push({x:300,y:height-115,width:100,height:10,color:"#a08f73"}),boxes.push({x:450,y:height-155,width:100,height:10,color:"#a08f73"}),boxes.push({x:700,y:height-155,width:100,height:10,color:"#a08f73"}),boxes.push({x:780,y:height-300,width:100,height:10,color:"#a08f73"}),boxes.push({x:1180,y:height-200,width:100,height:10,color:"#a08f73"}),lava.push({x:300,y:height-50,width:width-310,height:50,color:"#F76806"}),canvas.width=width,canvas.height=height,document.body.addEventListener("keydown",function(e){keys[e.code]=!0}),document.body.addEventListener("keyup",function(e){keys[e.code]=!1}),window.addEventListener("load",function(){update()});
+(function () {
+    var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+    window.requestAnimationFrame = requestAnimationFrame;
+})();
+
+document.getElementById("playGame").addEventListener('click', enterGame)
+
+function enterGame() {
+    document.getElementById('welcome').style.display = "none"
+}
+
+var canvas = document.getElementById("canvas"),
+    ctx = canvas.getContext("2d"),
+    width = document.body.clientWidth,
+    height = document.body.clientHeight,
+    player = {
+        x: 50,
+        y: height - 200,
+        width: 50,
+        height: 50,
+        speed: 3,
+        velX: 0,
+        velY: 0,
+        jumping: false,
+        grounded: false,
+        color: '#E6AC27',
+        image: document.getElementById('character')
+    },
+    level = 1,
+    keys = [],
+    friction = 0.8,
+    gravity = 0.4,
+    boxes = [],
+    boxes2 = [],
+    powerup = [],
+    lava = [];
+
+powerup.push({
+    x: 490,
+    y: height - 195,
+    width: 20,
+    height: 20,
+    color: '#BF4D28',
+    effect: 'shrink'
+});
+/*
+powerup.push({
+    x: 400,
+    y: 150,
+    width: 20,
+    height: 20,
+    color: '#BF4D28',
+    effect: 'gravity'
+});
+powerup.push({
+    x: -15,
+    y: 88,
+    width: 20,
+    height: 20,
+    color: '#222',
+    effect: 'tele',
+    rotate: 20,
+    px: 20,
+    py: 370,
+    stay: true
+});
+*/
+powerup.push({
+    x: 1200,
+    y: 415,
+    width: 20,
+    height: 20,
+    color: '#2A5D77',
+    effect: 'win',
+});
+
+
+boxes.push({
+    x: 0,
+    y: 0,
+    width: 10,
+    height: height,
+    color: '#a08f73',
+});
+boxes.push({
+    x: 0,
+    y: height - 50,
+    width: 300,
+    height: 50,
+    color: '#a08f73',
+});
+boxes.push({
+    x: width - 10,
+    y: 0,
+    width: 50,
+    height: height,
+    color: '#a08f73',
+});
+boxes.push({
+    x: 0,
+    y: 0,
+    width: width,
+    height: 10,
+    color: '#a08f73',
+})
+
+boxes.push({
+    x: 300,
+    y: height - 115,
+    width: 100,
+    height: 10,
+    color: '#a08f73',
+});
+
+boxes.push({
+    x: 450,
+    y: height - 155,
+    width: 100,
+    height: 10,
+    color: '#a08f73',
+});
+
+boxes.push({
+    x: 700,
+    y: height - 155,
+    width: 100,
+    height: 10,
+    color: '#a08f73',
+});
+
+boxes.push({
+    x: 780,
+    y: height - 300,
+    width: 100,
+    height: 10,
+    color: '#a08f73',
+});
+
+boxes.push({
+    x: 1160,
+    y: height - 200,
+    width: 100,
+    height: 10,
+    color: '#a08f73',
+});
+
+lava.push({
+    x: 300,
+    y: height - 50,
+    width: width - 310,
+    height: 50,
+    color: '#F76806'
+})
+
+
+canvas.width = width;
+canvas.height = height;
+
+function update() {
+
+    if (keys['ArrowUp'] || keys['Space'] || keys['KeyW']) {
+
+        if (!player.jumping && player.grounded) {
+            player.jumping = true;
+            player.grounded = false;
+            player.velY = -player.speed * 2.5;
+        }
+    }
+    if (keys['ArrowRight'] || keys['KeyD']) {
+
+        if (player.velX < player.speed) {
+            player.velX++;
+        }
+    }
+    if (keys['ArrowLeft'] || keys['KeyA']) {
+
+        if (player.velX > -player.speed) {
+            player.velX--;
+        }
+    }
+
+
+
+    player.velX *= friction;
+    player.velY += gravity;
+
+    ctx.clearRect(0, 0, width, height);
+
+
+    player.grounded = false;
+    ctx.beginPath();
+
+    if (level == 1) {
+
+        ctx.font = 'normal 40px Montserrat';
+        ctx.fillText('Level 1', canvas.width / 2 - 50, canvas.height / 2 - 200)
+
+
+        for (var i = 0; i < boxes.length; i++) {
+            ctx.fillStyle = boxes[i].color;
+            ctx.fillRect(boxes[i].x, boxes[i].y, boxes[i].width, boxes[i].height);
+
+            var dir = collisionCheck(player, boxes[i]);
+
+            if (dir === "l" || dir === "r") {
+                player.velX = 0;
+                player.jumping = false;
+            } else if (dir === "b") {
+                player.grounded = true;
+                player.jumping = false;
+            } else if (dir === "t") {
+                player.velY *= -1;
+            }
+
+        }
+
+        for (var k = 0; k < lava.length; k++) {
+            ctx.fillStyle = lava[k].color;
+            ctx.fillRect(lava[k].x, lava[k].y, lava[k].width, lava[k].height)
+
+
+            var dir = collisionCheck(player, lava[k])
+            if (dir) {
+                console.log('touched')
+                player.x = 50
+                player.y = height - 200
+                alert('You Died!')
+                document.location.reload()
+            }
+        }
+
+        if (player.grounded) {
+            player.velY = 0;
+        }
+
+        player.x += player.velX;
+        player.y += player.velY;
+
+        ctx.drawImage(player.image, player.x, player.y, player.width, player.height);
+
+
+        for (var j = 0; j < powerup.length; j++) {
+            ctx.save();
+            var cx = powerup[j].x + 0.5 * powerup[j].width,
+                cy = powerup[j].y + 0.5 * powerup[j].height;
+            ctx.translate(cx, cy);
+            ctx.rotate((Math.PI / 180) * 45);
+            if (powerup[j].effect === 'tele') {
+                ctx.rotate((Math.PI / 180) * powerup[j].rotate);
+                powerup[j].rotate = (Math.PI / 180) * powerup[j].rotate;
+            }
+            ctx.translate(-cx, -cy);
+            ctx.fillStyle = powerup[j].color;
+            ctx.fillRect(powerup[j].x, powerup[j].y, powerup[j].width, powerup[j].height);
+            ctx.restore();
+
+
+            if (collisionCheck(player, powerup[j]) !== null) {
+                if (powerup[j].effect === 'gravity') {
+                    gravity = 0.4;
+                    player.speed = 4;
+                    player.color = 'white';
+                } else if (powerup[j].effect === 'shrink') {
+                    player.width = 10;
+                    player.height = 10;
+                    player.speed = 5;
+                } else if (powerup[j].effect === 'tele') {
+                    player.x = powerup[j].px;
+                    player.y = powerup[j].py;
+                } else if (powerup[j].effect === 'win') {
+                    var r = alert(`You beat level 1!`);
+                    r;
+                    player.x = 50
+                    player.y = height - 200
+                    player.width = 50;
+                    player.height = 50;
+                    player.speed = 3;
+                    player.velX = 0;
+                    player.velY = 0;
+                    player.jumping == false;
+                    //level = 2;
+
+                }
+                if (powerup[j].stay !== true)
+                    powerup[j].width = 0;
+                powerup[j].height = 0;
+            }
+        }
+
+
+        requestAnimationFrame(update);
+    }
+
+/*
+    if (level == 2) {
+        ctx.font = 'Montserrat sans-serif'
+        ctx.fillText('Level 2', canvas.width / 2, canvas.height / 2 - 100)
+
+        for (var i = 0; i < boxes.length; i++) {
+            ctx.fillStyle = boxes[i].color;
+            ctx.fillRect(boxes[i].x, boxes[i].y, boxes[i].width, boxes[i].height);
+
+            var dir = collisionCheck(player, boxes[i]);
+
+            if (dir === "l" || dir === "r") {
+                player.velX = 0;
+                player.jumping = false;
+            } else if (dir === "b") {
+                player.grounded = true;
+                player.jumping = false;
+            } else if (dir === "t") {
+                player.velY *= -1;
+            }
+
+        }
+
+        for (var k = 0; k < lava.length; k++) {
+            ctx.fillStyle = lava[k].color;
+            ctx.fillRect(lava[k].x, lava[k].y, lava[k].width, lava[k].height)
+
+
+            var dir = collisionCheck(player, lava[k])
+            if (dir) {
+                player.x = 50
+                player.y = height - 200
+                alert('You Died!')
+                document.location.reload()
+            }
+        }
+
+        if (player.grounded) {
+            player.velY = 0;
+        }
+
+        player.x += player.velX;
+        player.y += player.velY;
+
+        ctx.drawImage(player.image, player.x, player.y, player.width, player.height);
+
+
+        for (var j = 0; j < powerup.length; j++) {
+            ctx.save();
+            var cx = powerup[j].x + 0.5 * powerup[j].width,
+                cy = powerup[j].y + 0.5 * powerup[j].height;
+            ctx.translate(cx, cy);
+            ctx.rotate((Math.PI / 180) * 45);
+            if (powerup[j].effect === 'tele') {
+                ctx.rotate((Math.PI / 180) * powerup[j].rotate);
+                powerup[j].rotate = (Math.PI / 180) * powerup[j].rotate;
+            }
+            ctx.translate(-cx, -cy);
+            ctx.fillStyle = powerup[j].color;
+            ctx.fillRect(powerup[j].x, powerup[j].y, powerup[j].width, powerup[j].height);
+            ctx.restore();
+
+
+            if (collisionCheck(player, powerup[j]) !== null) {
+                if (powerup[j].effect === 'gravity') {
+                    gravity = 0.4;
+                    player.speed = 4;
+                    player.color = 'white';
+                } else if (powerup[j].effect === 'shrink') {
+                    player.width = 10;
+                    player.height = 10;
+                    player.speed = 5;
+                } else if (powerup[j].effect === 'tele') {
+                    player.x = powerup[j].px;
+                    player.y = powerup[j].py;
+                } else if (powerup[j].effect === 'win') {
+                    var r = confirm("You beat the level! Would you like to play again?");
+                    if (r == true) {
+                        document.location.reload();
+                    } else {
+                        player.x = 50
+                        player.y = height - 200
+                    }
+                }
+                if (powerup[j].stay !== true)
+                    powerup[j].width = 0;
+                powerup[j].height = 0;
+            }
+        }
+
+
+        requestAnimationFrame(update);
+    }
+    */
+}
+
+function collisionCheck(shapeA, shapeB) {
+
+    var vX = (shapeA.x + (shapeA.width / 2)) - (shapeB.x + (shapeB.width / 2)),
+        vY = (shapeA.y + (shapeA.height / 2)) - (shapeB.y + (shapeB.height / 2)),
+
+        hWidths = (shapeA.width / 2) + (shapeB.width / 2),
+        hHeights = (shapeA.height / 2) + (shapeB.height / 2),
+        colDir = null;
+
+
+    if (Math.abs(vX) < hWidths && Math.abs(vY) < hHeights) {
+
+        var oX = hWidths - Math.abs(vX),
+            oY = hHeights - Math.abs(vY);
+        if (oX >= oY) {
+            if (vY > 0) {
+                colDir = "t";
+                shapeA.y += oY;
+            } else {
+                colDir = "b";
+                shapeA.y -= oY;
+            }
+        } else {
+            if (vX > 0) {
+                colDir = "l";
+                shapeA.x += oX;
+            } else {
+                colDir = "r";
+                shapeA.x -= oX;
+            }
+        }
+    }
+    return colDir;
+}
+
+document.body.addEventListener("keydown", function (e) {
+    keys[e.code] = true;
+});
+
+document.body.addEventListener("keyup", function (e) {
+    keys[e.code] = false;
+});
+
+
+window.addEventListener("load", function () {
+    update();
+});
